5ef655c4c73932516db040ae866441e7d2efa920
jash2504 <asmanijash51@gmail.com>
2023-05-13 22:57:17 +0530

    Update signup.java

--- signup.java
+++ signup.java
@@ -72,7 +72,7 @@ public class signup extends HttpServlet {
 
                     pstmt.executeUpdate();
 
-                    out.println("<h1>sign Up Successfuly</h1>");
+                    out.println("<h1>Sign Up Successfuly</h1>");
 
                     pstmt.close();
 
